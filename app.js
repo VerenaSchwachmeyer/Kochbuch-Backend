f32f83dc442f2565e84ccd8cd55a0e19405a8965
VerenaSchwachmeyer <Vronus@gmx.de>
2023-03-31 16:25:11 +0200

    added/uncommented cors

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 const express = require("express");
 const app = express();
 const categories = require("./routes/categories");
-// const cors = require("cors");
+const cors = require("cors");
 
-// // whitelist
-// const whitelist = ["http://localhost:3000", "http://localhost:3001"];
+// whitelist
+const whitelist = ["http://localhost:3000", "http://localhost:3001"];
 
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   },
-// };
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
